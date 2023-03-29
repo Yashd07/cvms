8c530a3d8dda2cf4e86d02cfa943302f1887cf88
Yashd07 <yashd1272@gmail.com>
2023-03-29 23:16:38 +0530

    my commit

---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+<div class="row">
+                            <div class="col-md-12">
+                                <div class="copyright">
+                                    <p>Company Visitors Management System. Developed by Yash Desai</p>
+                                </div>
+                            </div>
+                        </div>
